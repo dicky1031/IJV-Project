f322da584544d877b8ed90b335c24420fc4c1657
dicky1031 <dicky10311111@gmail.com>
2023-10-30 00:31:01 +0800

    Update S1_make_folder.py

--- surrogate_model/S1_make_folder.py
+++ surrogate_model/S1_make_folder.py
@@ -3,6 +3,8 @@ import os
 subject = "Julie"
 
 #%% make directory for training folder and testing folder of subject
-os.makedirs(os.path.join("dataset", subject, 'train'), exist_ok=True)
-os.makedirs(os.path.join("dataset", subject, 'test'), exist_ok=True)
+os.makedirs(os.path.join("dataset", subject, 'train', 'ijv_large'), exist_ok=True)
+os.makedirs(os.path.join("dataset", subject, 'train', 'ijv_small'), exist_ok=True)
+os.makedirs(os.path.join("dataset", subject, 'test', 'ijv_large'), exist_ok=True)
+os.makedirs(os.path.join("dataset", subject, 'test', 'ijv_small'), exist_ok=True)
 
