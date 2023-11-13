c98b01b29400b18fb6185bc071ad163cf39447f6
dicky1031 <dicky10311111@gmail.com>
2023-11-13 15:47:26 +0800

    sync

--- mcx_sim/Run_MCX_Sim.sh
+++ mcx_sim/Run_MCX_Sim.sh
@@ -12,17 +12,17 @@ num_cca_mua=9
 root="Julie_low_scatter_v2"
 subject="Julie"
 date="20231012"
-PhotonNum=10000000
+PhotonNum=1000000000
 VoxelSize=0.25
 ijv_type_set=("ijv_large" "ijv_small")
-train_sim_start=1
-train_sim_end=1
 cvThreshold=2.5
 repeatTimes=10
 NA_enable=1
 NA=0.22
 # ====================================================================== #
 
+train_sim_start=1
+train_sim_end=$(($num_skin_mus*$num_fat_mus*$num_muscle_mus*$num_blood_mus))
 test_sim_start=1
 test_sim_end=$(($num_skin_mus*$num_fat_mus*$num_muscle_mus*$num_blood_mus/10)) # using 90% train 10% test
 
@@ -32,7 +32,7 @@ echo "Total mus combination conditions will be $(($num_skin_mus*$num_fat_mus*$nu
 echo "Total mua combination conditions will be $(($num_skin_mua*$num_fat_mua*$num_muscle_mua*$num_ijv_mua*$num_cca_mua))"
 echo "Total conditions will be $(($num_skin_mus*$num_fat_mus*$num_muscle_mus*$num_blood_mus*$num_skin_mua*$num_fat_mua*$num_muscle_mua*$num_ijv_mua*$num_cca_mua))"
 
-python .\S1_make_simarr.py --num_skin_mus $num_skin_mus --num_fat_mus $num_fat_mus --num_muscle_mus $num_muscle_mus --num_blood_mus $num_blood_mus --num_skin_mus $num_skin_mua --num_fat_mua $num_fat_mua --num_muscle_mua $num_muscle_mua --num_ijv_mua $num_ijv_mua --num_cca_mua $num_cca_mua
+python S1_make_simarr.py --num_skin_mus $num_skin_mus --num_fat_mus $num_fat_mus --num_muscle_mus $num_muscle_mus --num_blood_mus $num_blood_mus --num_skin_mua $num_skin_mua --num_fat_mua $num_fat_mua --num_muscle_mua $num_muscle_mua --num_ijv_mua $num_ijv_mua --num_cca_mua $num_cca_mua
 echo "################################### finish S1 ################################################"
 
 
@@ -44,7 +44,7 @@ echo "photon number used: $PhotonNum"
 echo "voxelsize: $VoxelSize"
 echo "If considering NA, the fiber NA is: $NA"
 
-python .\S2_make_simfile.py --root $root --subject $subject --date $data --PhotonNum $PhotonNum --VoxelSize $VoxelSize --NA $NA
+python S2_make_simfile.py --root $root --subject $subject --date $date --PhotonNum $PhotonNum --VoxelSize $VoxelSize --NA $NA
 echo "################################### finish S2 ################################################"
 
 
@@ -60,14 +60,14 @@ echo "Considering NA or not: $NA_enable"
 echo "If considering NA, the fiber NA is: $NA"
 
 for ijv_type in "${ijv_type_set[@]}"; do
-python .\S3_run_sim.py --root $root --subject $subject --ijv_type $ijv_type --start $train_sim_start --end $train_sim_end --cvThreshold $cvThreshold --repeatTimes $repeatTimes --datatype train --NA_enable $NA_enable --NA $NA
-python .\S3_run_sim.py --root $root --subject $subject --ijv_type $ijv_type --start $test_sim_start --end $test_sim_end --cvThreshold $cvThreshold --repeatTimes $repeatTimes --datatype test --NA_enable $NA_enable --NA $NA
+# python S3_run_sim.py --root $root --subject $subject --ijv_type $ijv_type --start $train_sim_start --end $train_sim_end --cvThreshold $cvThreshold --repeatTimes $repeatTimes --datatype train --NA_enable $NA_enable --NA $NA
+python S3_run_sim.py --root $root --subject $subject --ijv_type $ijv_type --start $test_sim_start --end $test_sim_end --cvThreshold $cvThreshold --repeatTimes $repeatTimes --datatype test --NA_enable $NA_enable --NA $NA
 done
 echo "################################### finish S3 ################################################"
 
-echo "################################### run S4 WMC ################################################"
-for ijv_type in "${ijv_type_set[@]}"; do
-python .\S4_wmc_generate_dataset.py --root $root --subject $subject --start $train_sim_start --end $train_sim_end --datatype train --ijv_type $ijv_type
-python .\S4_wmc_generate_dataset.py --root $root --subject $subject --start $test_sim_start --end $test_sim_end --datatype test --ijv_type $ijv_type
+# echo "################################### run S4 WMC ################################################"
+# for ijv_type in "${ijv_type_set[@]}"; do
+# python S4_wmc_generate_dataset.py --root $root --subject $subject --start $train_sim_start --end $train_sim_end --datatype train --ijv_type $ijv_type
+# python S4_wmc_generate_dataset.py --root $root --subject $subject --start $test_sim_start --end $test_sim_end --datatype test --ijv_type $ijv_type
 
-echo "################################### finish S4 ################################################"
+# echo "################################### finish S4 ################################################"
